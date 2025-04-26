df41feabf68460f1fca935774b547e3fbd8a866d
Jesper N <jesper.nilsson@omegapoint.se>
2025-04-26 21:06:16 +0200

    Update keybinds and lsp

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,8 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set('n', 't', 'o<ESC>k')
+vim.keymap.set('n', 'T', 'O<ESC>k')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
